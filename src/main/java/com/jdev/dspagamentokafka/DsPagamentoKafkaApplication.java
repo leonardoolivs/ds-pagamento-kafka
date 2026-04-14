2edfe4f6afa9f1fd689d7b192f3566bddbd42ab5
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-13 21:45:57 -0300

    build: Cria o projeto com DevTools, H2, Spring Web, Lombok, Validation e JPA

---
+++ src/main/java/com/jdev/dspagamentokafka/DsPagamentoKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.jdev.dspagamentokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsPagamentoKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsPagamentoKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jdev/dspagamentokafka/DsPagamentoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdev.dspagamentokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsPagamentoKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
