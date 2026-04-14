6e8295f0facf3ff20447c267c0d2ab794cc0dad7
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-14 00:50:19 -0300

    feat: Cria DTO para Boleto

---
+++ src/main/java/com/jdev/dspagamentokafka/dtos/BoletoDTO.java
@@ -0,0 +1,34 @@
+package com.jdev.dspagamentokafka.dtos;
+
+import com.jdev.dspagamentokafka.entity.Boleto;
+import com.jdev.dspagamentokafka.enums.Situacao;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoletoDTO {
+
+    private Long id;
+    private String codigoBarras;
+    private Situacao situacao;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataAtualizacao;
+
+    public static BoletoDTO toDTO(Boleto entity) {
+        return new BoletoDTO(
+                entity.getId(),
+                entity.getCodigoBarras(),
+                entity.getSituacao(),
+                entity.getDataCriacao(),
+                entity.getDataAtualizacao()
+        );
+    }
+}
